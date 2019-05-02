8dd1bbe6c5bcce2f69d60566ced8798531fddd56
ainiml <naisanza@gmail.com>
2019-05-02 04:22:32 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# dojjer
+
+Docker, docker, on the wall
+
+who's the dojjiest of it all
+
--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 
 cd $(dirname $0)
 
+if [ ! $(which docker) ]; then echo "Missing docker"; exit 1; fi
 if [ ! -f Dockerfile ]; then echo "Missing Dockerfile"; exit 1; fi
+
 
 STR=$(cat Dockerfile | grep LABEL | grep dockername) || { echo "failed"; exit 1; }
 DOCKERNAME=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit 1; }
