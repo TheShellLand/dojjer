28a8af68a8f4033707d47e2137694f041f44117f
ainiml <ericjaw@gmail.com>
2019-05-03 16:10:40 -0400

    added new metatag: dockertag

--- build.sh
+++ build.sh
@@ -4,15 +4,24 @@
 
 cd $(dirname $0)
 
-if [ ! $(which docker) ]; then echo "Missing docker"; exit 1; fi
-if [ ! -f Dockerfile ]; then echo "Missing Dockerfile"; exit 1; fi
 
-STR=$(cat Dockerfile | grep LABEL | grep dockername) || { echo "failed"; exit 1; }
-DOCKERNAME=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit 1; }
+DOCKERFILE="Dockerfile"
+
+if [ ! $(which docker) ]; then echo "*** missing docker, please install docker ***"; exit 1; fi
+if [ ! -f $DOCKERFILE ]; then echo "*** missing $DOCKERFILE ***"; exit 1; fi
+
+
+# get dockername
+STR=$(cat $DOCKERFILE | grep LABEL | grep dockername) || { echo "failed"; exit 1; }
+DOCKERNAME=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit   1; }
+
+# get dockertag
+STR=$(cat $DOCKERFILE | grep LABEL | grep dockertag) || { echo "failed"; exit 1; }
+DOCKERTAG=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit    1; }
 
 # build image
-docker build -t $DOCKERNAME .
+docker build -t $DOCKERNAME:$DOCKERTAG .
 
 # push image
-docker push $DOCKERNAME
+docker push $DOCKERNAME:$DOCKERTAG
 
--- templates/bash/Dockerfile
+++ templates/bash/Dockerfile
@@ -2,7 +2,8 @@ FROM ubuntu:18.04
 
 LABEL maintainer="naisanza@gmail.com"
 LABEL description="dojjer template"
-LABEL dockername="theshellland/dojjer:bash"
+LABEL dockername="theshellland/dojjer"
+LABEL dockertag="bash"
 LABEL version="0.1"
 
 
