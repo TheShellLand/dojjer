c6b4fbb45ae1d27dbf84ad977e38bd17e5d92581
ainiml <ericjaw@gmail.com>
2020-08-23 05:53:03 -0400

    update template

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM ubuntu:18.04
 
 LABEL maintainer="naisanza@gmail.com"
 LABEL description="dojjer template"
-LABEL dockername="theshellland/dojjer"
-LABEL dockertag="bash"
 
 WORKDIR /root
 
--- build.sh
+++ build.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-# docker build script
-
-cd $(dirname $0)
-
-DOCKERFILE="Dockerfile"
-
-if [ ! $(which docker) ]; then echo "*** missing docker, please install docker ***"; exit 1; fi
-if [ ! -f $DOCKERFILE ]; then echo "*** missing $DOCKERFILE ***"; exit 1; fi
-
-# get dockername
-STR=$(cat $DOCKERFILE | grep LABEL | grep dockername) || { echo "failed"; exit 1; }
-DOCKERNAME=$(echo "$STR" | cut -d '=' -f 2 | cut -d ' ' -f 3 | sed  's/"//g') || { echo "failed"; exit 1; }
-
-# get dockertag
-DOCKERTAG=$(git describe --tags)
+# dojjer build script
 
 set -xe
+cd $(dirname $0)
+
+DOCKERNAME=theshellland/dojjer:bash
+DOCKERTAG=$(git describe --tag --always)
 
 # build image
 docker build -t $DOCKERNAME:$DOCKERTAG .
 
+# list image
+docker images $DOCKERNAME
+
 # push image
 docker push $DOCKERNAME:$DOCKERTAG
 
-# list image
-docker images | grep $DOCKERNAME
